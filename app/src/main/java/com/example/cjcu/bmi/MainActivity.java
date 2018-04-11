bffda765bf355ba86556fe9d9ce4784948d2e5e3
ts60325tw <ts60325tw@yahoo.com.tw>
2018-04-11 15:59:09 +0800

    第四次練習(Intent)

--- app/src/main/java/com/example/cjcu/bmi/MainActivity.java
+++ app/src/main/java/com/example/cjcu/bmi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cjcu.bmi;
 
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -45,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
                 .setPositiveButton("確定",null)
                 .setNegativeButton("CANCEL",null)
                 .show();
+
+        Intent intent = new Intent(this,ResultActivity.class);
+        startActivity(intent);
     }
 
 }
